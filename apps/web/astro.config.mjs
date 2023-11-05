8acc68e9ded84cd0eefb86c638b211d2089a2dce
Eilam R <eilam.craft@gmail.com>
2023-11-05 16:49:49 +0000

    apps/web: enable Vercel SSR adapter

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://clown-tunes.vercel.app",
+  output: "server",
+  adapter: vercel(),
   integrations: [tailwind({ applyBaseStyles: false })],
 });
