cb7a6c4490bc3b54581f58dab10bf9a907dd4455
Eilam Rosenfeld <circus.real.dev@gmail.com>
2023-11-24 11:35:22 +0000

    Update utils.ts

--- apps/web/src/utils.ts
+++ apps/web/src/utils.ts
@@ -1,6 +1,8 @@
+const base: string = "https://supersonic.software/music"
+
 export async function getArtists() {
   const artists: { name: string }[] = await fetch(
-    "https://supersonic.software/music/.cal/meta/artists.json",
+    `${base}/.cal/meta/artists.json`,
     {
       headers: {
         "Content-Type": "application/json",
@@ -15,7 +17,7 @@ export async function getArtists() {
 
 export async function getAlbums(artist: string) {
   const albums: { name: string }[] = await fetch(
-    `https://supersonic.software/music/.cal/albums/${artist}_albums.json`,
+    `${base}/.cal/albums/${artist}_albums.json`,
     {
       headers: {
         "Content-Type": "application/json",
@@ -30,7 +32,7 @@ export async function getAlbums(artist: string) {
 
 export async function getSongs(artist: string, album: string) {
   const songs: { name: string; path: string }[] = await fetch(
-    `https://supersonic.software/music/.cal/songs/${artist}_${album}_songs.json`,
+    `${base}/.cal/songs/${artist}_${album}_songs.json`,
     {
       headers: {
         "Content-Type": "application/json",
